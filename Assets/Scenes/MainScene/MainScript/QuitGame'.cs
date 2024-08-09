c5fab8e400e1a4abf8931debab22aaf1e095db51
auto2557 <149247473+auto2557@users.noreply.github.com>
2024-08-09 22:23:39 +0700

    Add-Audio-Text

---
+++ Assets/Scenes/MainScene/MainScript/QuitGame'.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(QuitGame);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
